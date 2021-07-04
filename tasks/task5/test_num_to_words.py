1fc4abfb44a6b6961b90b7632f4a99ab0fe070ab
SKyzjBpN <sensitivestory@gmail.com>
2021-07-04 13:16:55 +0300

    Added tests for task5

--- tasks/task5/test_num_to_words.py
+++ tasks/task5/test_num_to_words.py
@@ -1,6 +1,6 @@
 import pytest
 
-from .num_to_words import NumberToWords
+from .num_to_words import *
 
 
 @pytest.fixture()
@@ -12,5 +12,18 @@ def test_get_digits(num_to_words):
     assert num_to_words.get_digits(100)
 
 
-def test_number_to_words(num_to_words):
-    assert num_to_words.number_to_words(100890)
+def test_user_input(mocker):
+    mocker.patch('argparse.ArgumentParser.parse_args', return_value=argparse.Namespace(integer=100890))
+    res = get_number()
+    assert res == 100890
+
+
+def numbers():
+    yield from [0, 110, 21049, 93000, 612001, 3000000, 211000000, 501000067, 917552879]
+
+
+@pytest.mark.parametrize("number", numbers())
+def test_num_to_words(number):
+    assert NumberToWords().number_to_words(number)
+
+
