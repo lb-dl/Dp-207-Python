035830690ae5e75c4b44a06c17a80e0b10a45806
SKyzjBpN <sensitivestory@gmail.com>
2021-07-07 23:00:47 +0300

    Refactored

--- tasks/task3/triangles.py
+++ tasks/task3/triangles.py
@@ -1,45 +1,66 @@
 from math import sqrt
 
-INSTRUCTIONS = 'Enter a name and three sides of a triangle, separated by commas "," (e.g "name, 4, 5, 6") \n'
-HELP_MSG = 'The triangle sides must be numbers greater than 0. Try again, please'
+INSTRUCTIONS = 'Enter a name and three sides of a triangle, separated by commas "," (e.g "triangle, 4, 5, 6") \n'
 OUTPUT_STR = '============= Triangles list: ==============='
 NOT_EXISTED_TRIANGLE = 'There is not a triangle with entered sides'
-MESSAGE = 'Would you like to continue? Enter "yes" or "y" to continue. Enter another button to stop \n'
+MESSAGE = 'Would you like to continue? Enter "yes" or "y" to continue. Press another button to quit \n'
 INVALID_DATA_MSG = 'Entered data is incorrect. Try again, please'
 
 
 def main():
+    """
+    While a programme runs, the function creates a triangle, calling the method
+    'create_triangle' and appends it into a list, in case the triangle is not 'None'.
+    When the programme stops, the method 'sort_triangles' is called and a sorted
+    list of triangles is printed line by line.
+    """
     list_of_triangles = []
     run = True
     while run:
-        try:
-            user_input = get_user_input()
-            if user_input:
-                values = user_input.split(',')
-                if len(values) != 4:
-                    print(INVALID_DATA_MSG)
-                else:
-                    triangle_name = values[0]
-                    side_a = float(values[1])
-                    side_b = float(values[2])
-                    side_c = float(values[3])
-                    if validation(side_a) and validation(side_b) and validation(side_c):
-                        triangle = Triangle(triangle_name, side_a, side_b, side_c)
-                        if triangle.is_triangle():
-                            list_of_triangles.append(triangle)
-                        else:
-                            print(NOT_EXISTED_TRIANGLE)
-                    else:
-                        print(HELP_MSG)
-        except ValueError:
-            print(HELP_MSG)
+        a_triangle = create_triangle()
+        if a_triangle:
+            list_of_triangles.append(a_triangle)
         run = is_exit()
 
-    # sort triangles by their square
-    sorted_triangles = sorted(list_of_triangles, key=lambda x: x.get_area(), reverse=True)
     print(OUTPUT_STR)
-    for triangle in sorted_triangles:
+    list_of_sorted_triangles = sort_triangles(list_of_triangles)
+    for triangle in list_of_sorted_triangles:
         triangle.__repr__()
+
+
+def create_triangle():
+    """
+    The function asks for the user input. If the input data is valid,
+    an instance 'triangle' is created and returned. Else, the message about invalid data is printed.
+    """
+    user_input = get_user_input()
+    if user_input:
+        try:
+            values = user_input.split(',')
+            if len(values) != 4:
+                print(INVALID_DATA_MSG)
+            else:
+                triangle_name = values[0]
+                side_a = float(values[1])
+                side_b = float(values[2])
+                side_c = float(values[3])
+                if validation(side_a) and validation(side_b) and validation(side_c):
+                    triangle = Triangle(triangle_name, side_a, side_b, side_c)
+                    if triangle.is_triangle():
+                        return triangle
+                    else:
+                        print(NOT_EXISTED_TRIANGLE)
+                else:
+                    print(INVALID_DATA_MSG)
+                    pass
+        except ValueError:
+            print(INVALID_DATA_MSG)
+
+
+def sort_triangles(list_):
+    # sort triangles by their area
+    sorted_triangles = sorted(list_, key=lambda x: x.get_area(), reverse=True)
+    return sorted_triangles
 
 
 def get_user_input():
@@ -51,8 +72,8 @@ def get_user_input():
 
 def validation(value):
     """
-    The function checks and returns 'True' if the entered values are numbers greater than 0.
-    Otherwise, it prints a message with the further instructions and returns 'False'.
+    The function checks and returns 'True' if the entered values are numbers greater than 0
+    and the numbers' type is float. Otherwise, it returns 'False'.
     """
     return isinstance(value, float) and value > 0
 
@@ -79,9 +100,9 @@ class Triangle:
 
     def get_area(self):
 
-        # Counts and returns a square of a triangle
+        # Counts and returns a square of a triangle using Heron formula
         p = 0.5*(self.side_a + self.side_b + self.side_c)
-        self.area = round(sqrt(p*(p - self.side_a) * (p - self.side_b) * (p - self.side_c)))
+        self.area = round(sqrt(p*(p - self.side_a) * (p - self.side_b) * (p - self.side_c)), 2)
         return self.area
 
     def is_triangle(self):
--- tasks/tests/test_triangles.py
+++ tasks/tests/test_triangles.py
@@ -1,6 +1,6 @@
 import pytest
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 
 from ..task3.triangles import *
 
@@ -13,7 +13,7 @@ def triangles():
 
 
 def test_sorted_list(triangles):
-    expected_list = [292, 4]
+    expected_list = [292.42, 3.55]
     assert expected_list == sorted([triangles[0].get_area(), triangles[1].get_area()], reverse=True)
 
 
@@ -23,7 +23,11 @@ def triangle():
 
 
 def test_get_area(triangle):
-    assert triangle.get_area() == 11
+    assert triangle.get_area() == 10.69
+
+
+def test_get_area_invalid_result(triangle):
+    assert triangle.get_area() != 11
 
 
 def test_is_triangle(triangle):
@@ -43,3 +47,8 @@ def test_user_answer_yes():
 def test_user_input():
     with unittest.mock.patch('builtins.input', return_value='triangle1, 4.3,5.2,6.7'):
         assert get_user_input() == 'triangle1, 4.3,5.2,6.7'
+
+
+def test_sort_triangles():
+    test_list_of_triangles = [['tr1', 11.18], ['tr2', 9.12], ['tr3', 292.42], ['tr4', 3.55]]
+    assert sort_triangles(test_list_of_triangles) == []
